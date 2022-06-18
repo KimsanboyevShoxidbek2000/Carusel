649eea36324a0905f12d9c29807e5ecd932b86f2
Shoxidbek Kimsanboyev <shox7316@gmail.com>
2022-06-18 10:02:14 +0500

    master

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+//  7 va 11 ga bo'linadigon raqamlarni chiqarin
+
+for(let i = 1; i <= 100; i++){
+   if(i % 7 === 0 || i % 11 === 0){
+   console.log(i);
+   }
+}
